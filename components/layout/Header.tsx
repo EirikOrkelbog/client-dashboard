416b16c47d822c39db276e5f0eec804a80196d65
Eirik Orkelbog <eirikorkelbog@gmail.com>
2026-02-23 17:39:08 +0100

    added header layout

---
+++ components/layout/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+                <h1>Client Dashboard</h1>
+            <div className="container">
+                Login
+            </div>
+        </header>
+    );
+}
